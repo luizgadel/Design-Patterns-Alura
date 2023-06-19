7c282ebf629adb665da6fee7217c0a9418abe3a2
Luiz Gadelha <lpgds.eng16@uea.edu.br>
2023-06-19 13:38:21 -0400

    Padrão Strategy
    Interface "Imposto";
    Duas classes "ICCC" e "ICMS" que implementam "Imposto";
    Classe "CalculadorDeImpostos" onde está a função "RealizaCalculo" que recebe por parâmetro um objeto de uma classe que implementa "Imposto" e chama o método "Calcula" desse objeto.

---
+++ Imposto/ICCC.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoPadroesProjeto.Impostos
+{
+    public class ICCC : Imposto
+    {
+        public double Calcula(Orcamento orcamento)
+        {
+            double resultado = 0.0;
+            if (orcamento.Valor < 1000)
+            {
+                resultado += orcamento.Valor * 0.05;
+            }
+            else if (orcamento.Valor >= 1000 && orcamento.Valor <= 3000)
+            {
+                resultado += orcamento.Valor * 0.07;
+            }
+            else
+            {
+                resultado += orcamento.Valor * 0.08 + 30;
+            }
+
+            return resultado;
+        }
+    }
+}
