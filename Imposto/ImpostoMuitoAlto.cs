fbdd3ee7c552408a964f084a0832563089f9e06a
Luiz Gadelha <lpgds.eng16@uea.edu.br>
2023-06-26 17:20:32 -0400

    Feat: ImpostoMuitoAlto

---
+++ Imposto/ImpostoMuitoAlto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoPadroesProjeto.Impostos
+{
+    public class ImpostoMuitoAlto : Imposto
+    {
+        public ImpostoMuitoAlto() : base() { }
+
+        public ImpostoMuitoAlto(Imposto OutroImposto) : base(OutroImposto) { }
+
+        public override double Calcula(Orcamento orcamento)
+        {
+            return orcamento.Valor * 0.2 + CalculaOutroImposto(orcamento);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace CursoPadroesProjeto
         static void Main(string[] args)
         {
             Orcamento orcamento = new Orcamento(1000);
-            Imposto iss = new ISS(new ICMS(new ICCC()));
+            Imposto iss = new ImpostoMuitoAlto(new ICPP());
 
             double imposto = iss.Calcula(orcamento);
             Console.WriteLine(imposto);
